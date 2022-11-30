162c106ba887e2326498c54d6170b90c09847b75
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2022-11-30 12:13:43 +0100

    fix : correction de la class ennemies : race supprimé

--- src/personnages/ennemies.ts
+++ src/personnages/ennemies.ts
@@ -1,11 +1,9 @@
 import { Personnage } from "./personnage";
 
 export class Ennemies extends Personnage {
-    private race : string
-    constructor(name: string, health: number, strenght: number, lvl: number, exp: number, race: string) {
-        super(name, health, strenght, lvl, exp)
-        this.race = race
 
+    constructor(name: string, health: number, strenght: number, lvl: number, exp: number) {
+        super(name, health, strenght, lvl, exp)
     }
 
 }
