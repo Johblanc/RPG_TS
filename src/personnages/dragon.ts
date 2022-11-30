e015228b39fe9e975278651bfcb9fce26e7887dc
Titou <menardjc.33@gmail.com>
2022-11-30 16:38:18 +0100

    feat: polymorphisme de l'attack du dragon et l'ajout de phases

--- src/personnages/dragon.ts
+++ src/personnages/dragon.ts
@@ -3,6 +3,7 @@ import { Personnage } from "./personnage";
 
 export class dragon extends Ennemies {
     private isFlying: boolean
+    private phase: number = 1;
 
     constructor(name: string, health: number, strenght: number, lvl: number, exp: number, isFlying: boolean) {
         super(name, health, strenght, lvl, exp,)
@@ -37,8 +38,22 @@ export class dragon extends Ennemies {
         super.attack(opponent);
         this.setStrenght(initStrenght);
         this.isFlying = false;
+        
     }
-    
+        attack(opponent: Personnage): void {
+            if(this.phase == 1) {
+            super.attack(opponent); //phase 1
+        }
+        else if (this.phase == 2) {
+            this.fly(); // phase 2
+        }
+        else {
+            this.attackFromSky(opponent);// phase 3
+            this.phase = 0  // reinitaliser les phases
+        }
+        this.phase += 1   // passe à la phase suivante
+    }
+
 
 }
 
