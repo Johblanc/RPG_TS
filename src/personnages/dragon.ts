536cc9d8791f97f62c843194c402da15b78fe743
Titou <menardjc.33@gmail.com>
2022-11-30 15:00:12 +0100

    fix: class dragon

--- src/personnages/dragon.ts
+++ src/personnages/dragon.ts
@@ -4,9 +4,9 @@ import { Personnage } from "./personnage";
 export class dragon extends Ennemies {
     isFlying : boolean
 
-    constructor(name: string, health: number, strenght: number, lvl: number, exp: number) {
+    constructor(name: string, health: number, strenght: number, lvl: number, exp: number, isFlying : boolean) {
         super(name, health, strenght, lvl, exp,)
-        this.isFlying = this.isFlying
+        this.isFlying = isFlying;
     }
     fly(): void { }
     attackFromSky(opponent: Personnage): void { }
