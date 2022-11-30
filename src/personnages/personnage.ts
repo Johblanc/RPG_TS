f92c0e34b5cb62b092155ed9700d200ddd7859ea
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2022-11-30 11:35:54 +0100

    feat : création de la class personnage

---
+++ src/personnages/personnage.ts
@@ -0,0 +1,55 @@
+class Personnage {
+    private name: string;
+    private health: number;
+    private strenght: number;
+    private lvl: number;
+    private exp: number;
+    //race : string;
+
+    constructor(name: string, health: number, strenght: number, lvl: number, exp: number) {
+        this.name = name;
+        this.health = health;
+        this.strenght = strenght;
+        this.lvl = lvl;
+        this.exp = exp;
+
+    }
+
+    /** permet de récuperer le nom */
+    public getName(): string {
+        return this.name
+    }
+    public setName(newvalue: string): void {
+        this.name = newvalue;
+    }
+    /** permet de récupérer la vie */
+    public getHealth(): number {
+        return this.health
+    }
+    public setHealth(newvalue: number): void {
+        this.health = newvalue;
+    }
+    /** permet de récupérer la force */
+    public getStrenght(): number {
+        return this.strenght
+    }
+    public setStrenght(newvalue: number): void {
+        this.strenght = newvalue;
+    }
+    /** permet de récupérer le niveau */
+    public getLvl(): number {
+        return this.lvl
+    }
+    public setLvl(newvalue: number): void {
+        this.lvl = newvalue;
+    }
+    /** permet de récupérer l'expérience */
+    public getExp(): number {
+        return this.exp
+    }
+    public setExp(newvalue: number): void {
+        this.exp = newvalue;
+    }
+
+}
+
