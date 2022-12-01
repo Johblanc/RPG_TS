6e15e3f1be41979167762e5897176c7e8517ee4e
Titou <menardjc.33@gmail.com>
2022-12-01 15:28:43 +0100

    commit avant pull

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import { Hero } from "./personnages/hero";
 
 let morzi = new Hero("Morzileuille",100,12,1,0,"dwarf");
 let perleNord = new Hero ("Perle Nord", 100,11,1,0,"elf");
-let finnLame = new Assassin("Finn Lame", 80, 8, 1,0,false)
+let finnLame = new Assassin("Finn Lame", 80, 8, 1,0,false);
