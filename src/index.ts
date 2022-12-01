5f1e5ffba02f8345567b461b6bbd5097f3d8c3a7
Titou <menardjc.33@gmail.com>
2022-12-01 15:22:05 +0100

    feat: création de Perle Nord l'elfe et Finn Lame l'assassin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { Assassin } from "./personnages/assassin";
 import { Hero } from "./personnages/hero";
 
 
 
 let morzi = new Hero("Morzileuille",100,12,1,0,"dwarf");
+let perleNord = new Hero ("Perle Nord", 100,11,1,0,"elf");
+let finnLame = new Assassin("Finn Lame", 80, 8, 1,0,false)
