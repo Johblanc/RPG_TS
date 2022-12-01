1b43fdcf25cec8c501dcca956eddfbd578a120fe
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-12-01 15:21:21 +0100

    feat : ajout de Draco et tetede

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { Dragon } from "./personnages/dragon";
+import { Golem } from "./personnages/golem";
 import { Hero } from "./personnages/hero";
 
 
 
 let morzi = new Hero("Morzileuille",100,12,1,0,"dwarf");
+
+
+let draco = new Dragon("Draco",100,17,1,0,false);
+let tetede = new Golem("Tête de Pioche",150,12,1,0)
